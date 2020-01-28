ef3c83570608ab8ca040f63fefc3bfdbcbcf5811
James Leahy <defuncart@gmail.com>
2020-01-28 19:06:41 +0100

    feature/add localization (#2)
    Adds localization (DE, EN) for the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,4 @@
 import 'package:flutter/material.dart';
+import 'package:photos_sync/widgets/my_app.dart';
 
 void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Photos Sync'),
-        ),
-        body: Center(
-          child: Text('Photos Sync'),
-        ),
-      ),
-    );
-  }
-}
