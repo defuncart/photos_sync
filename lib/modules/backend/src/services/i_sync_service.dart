02664557531412d912bd5429f496f7f0a61c513a
James Leahy <defuncart@gmail.com>
2020-01-31 17:00:58 +0100

    Add IAuthService, IDatabaseService and ISyncService.

---
+++ lib/modules/backend/src/services/i_sync_service.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+import 'package:meta/meta.dart';
+
+import '../models/synced_photo.dart';
+
+/// A service which syncs files with a remote server
+abstract class ISyncService {
+  /// Uploads a file
+  Future<String> uploadFile(File file, {@required SyncedPhoto photo});
+
+  /// Determines a file's url
+  Future<String> getFileUrl(SyncedPhoto photo);
+
+  /// Downloads a file
+  Future<File> downloadFile(SyncedPhoto photo);
+
+  /// Deletes a file
+  Future<void> deleteFile(SyncedPhoto photo);
+}
