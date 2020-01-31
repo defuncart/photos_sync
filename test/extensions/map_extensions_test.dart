15e054984f2d3c211820dc365beaa13e74e60c01
James Leahy <defuncart@gmail.com>
2020-01-31 17:01:57 +0100

    Update map extensions tests.

--- test/extensions/map_extensions_test.dart
+++ test/extensions/map_extensions_test.dart
@@ -7,4 +7,28 @@ main() {
     final value = map.tryParse('key');
     expect(value, null);
   });
+
+  test('tryParse: Empty map', () {
+    final Map map = {};
+    final value = map.tryParse('key');
+    expect(value, null);
+  });
+
+  test('tryParse: Null key', () {
+    final Map map = {'key': 'myString'};
+    final value = map.tryParse(null);
+    expect(value, null);
+  });
+
+  test('tryParse: Incorrect key', () {
+    final Map map = {'key': 'myString'};
+    final value = map.tryParse('bla');
+    expect(value, null);
+  });
+
+  test('tryParse: Correct key', () {
+    final Map map = {'key': 'myString'};
+    final value = map.tryParse('key');
+    expect(value, 'myString');
+  });
 }
