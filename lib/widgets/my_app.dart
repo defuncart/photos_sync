d3c51bfcd98c8156df7d3cd8fadc4844e5144c11
James Leahy <defuncart@gmail.com>
2020-02-02 12:16:40 +0100

    Setup dependency injection.

--- lib/widgets/my_app.dart
+++ lib/widgets/my_app.dart
@@ -1,24 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
 
 import 'package:photos_sync/i18n.dart';
+import 'package:photos_sync/modules/backend/backend.dart';
+import 'package:photos_sync/services/firebase_service.dart';
 
 class MyApp extends StatelessWidget {
+  static final _firebaseService = FirebaseService();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: [
-        const I18nDelegate(),
-        GlobalCupertinoLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: I18nDelegate.supportedLocals,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Photos Sync'),
+    return MultiProvider(
+      providers: [
+        Provider<IAuthService>.value(
+          value: _firebaseService,
         ),
-        body: _HomeScreen(),
+        Provider<ISyncService>.value(
+          value: _firebaseService,
+        ),
+        Provider<IDatabaseService>.value(
+          value: _firebaseService,
+        ),
+      ],
+      child: MaterialApp(
+        localizationsDelegates: [
+          const I18nDelegate(),
+          GlobalCupertinoLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: I18nDelegate.supportedLocals,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Photos Sync'),
+          ),
+          body: _HomeScreen(),
+        ),
       ),
     );
   }
