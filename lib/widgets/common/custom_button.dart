dc99dd138bd9a03ae24f7a861b37f3019d17ba5e
James Leahy <defuncart@gmail.com>
2020-08-14 11:02:53 +0200

    Add CustomButton.

---
+++ lib/widgets/common/custom_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String buttonText;
+  final void Function() onPressed;
+  final bool isFilled;
+
+  const CustomButton({
+    @required this.buttonText,
+    @required this.onPressed,
+    this.isFilled = true,
+    Key key,
+  })  : assert(buttonText != null),
+        assert(onPressed != null),
+        assert(isFilled != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      color: isFilled ? Colors.black : Colors.white,
+      shape: RoundedRectangleBorder(
+        side: BorderSide(
+          color: Colors.black,
+          width: 2,
+          style: BorderStyle.solid,
+        ),
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      child: Text(
+        buttonText.toUpperCase(),
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      textColor: isFilled ? Colors.white : Colors.black,
+      onPressed: onPressed,
+      minWidth: MediaQuery.of(context).size.width * 0.75,
+    );
+  }
+}
