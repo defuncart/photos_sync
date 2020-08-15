fc9de6ffa25862fb213d198a6e9a78ef802b8503
James Leahy <defuncart@gmail.com>
2020-08-15 16:30:48 +0200

    Update WelcomeScreen to direct to LoginScreen/SignupScreen.

--- lib/widgets/welcome_screen/welcome_screen.dart
+++ lib/widgets/welcome_screen/welcome_screen.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:photos_sync/configs/route_names.dart';
 import 'package:photos_sync/i18n.dart';
-import 'package:photos_sync/modules/backend/backend.dart';
 import 'package:photos_sync/widgets/common/custom_button.dart';
-import 'package:photos_sync/widgets/common/modal_progress_indicator.dart';
-import 'package:provider/provider.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final authService = context.watch<IAuthService>();
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -64,26 +60,13 @@ class WelcomeScreen extends StatelessWidget {
               children: [
                 CustomButton(
                   buttonText: I18n.welcomeScreenSignupButtonText,
-                  onPressed: () async {
-                    ModalProgressIndicator.show(context);
-                    final success = await authService.createUserAccount(email: 'bla@test.de', password: '123456');
-                    ModalProgressIndicator.dismiss();
-                    if (success) {
-                      await Navigator.of(context).pushReplacementNamed(RouteNames.homeScreen);
-                    }
-                  },
+                  onPressed: () => Navigator.of(context).pushReplacementNamed(RouteNames.signupScreen),
                   isFilled: true,
                 ),
+                Container(height: 16),
                 CustomButton(
                   buttonText: I18n.welcomeScreenLoginButtonText,
-                  onPressed: () async {
-                    ModalProgressIndicator.show(context);
-                    final success = await authService.login(email: 'test@test.de', password: '123456');
-                    ModalProgressIndicator.dismiss();
-                    if (success) {
-                      await Navigator.of(context).pushReplacementNamed(RouteNames.homeScreen);
-                    }
-                  },
+                  onPressed: () => Navigator.of(context).pushReplacementNamed(RouteNames.loginScreen),
                   isFilled: false,
                 ),
                 Container(height: 16),
