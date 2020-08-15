7cc3b6eaf0af2aad57cd14a254b56abe09433aad
James Leahy <defuncart@gmail.com>
2020-08-15 16:30:26 +0200

    Add LoginScreen, SignupScreen.

---
+++ lib/widgets/auth/login_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:photos_sync/configs/route_names.dart';
+import 'package:photos_sync/i18n.dart';
+import 'package:photos_sync/modules/backend/backend.dart';
+import 'package:photos_sync/modules/user_preferences/user_preferences.dart';
+import 'package:photos_sync/widgets/auth/auth_screen.dart';
+import 'package:photos_sync/widgets/common/modal_progress_indicator.dart';
+import 'package:provider/provider.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AuthScreen(
+      title: I18n.loginScreenTitle,
+      mainButtonText: I18n.loginScreenMainButtonText,
+      onMainButtonPressed: (email, password) async {
+        ModalProgressIndicator.show(context);
+        final success = await context.read<IAuthService>().login(email: email, password: password);
+        ModalProgressIndicator.dismiss();
+        if (success) {
+          await UserPreferences.setUsername(email);
+          await Navigator.of(context).pushReplacementNamed(RouteNames.homeScreen);
+        }
+      },
+      secondaryButtonText: I18n.loginScreenSecondaryButtonText,
+      onSecondaryButtonPressed: () => Navigator.of(context).pushReplacementNamed(RouteNames.signupScreen),
+    );
+  }
+}
---
+++ lib/widgets/auth/signup_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:photos_sync/configs/route_names.dart';
+import 'package:photos_sync/i18n.dart';
+import 'package:photos_sync/modules/backend/backend.dart';
+import 'package:photos_sync/modules/user_preferences/user_preferences.dart';
+import 'package:photos_sync/widgets/auth/auth_screen.dart';
+import 'package:photos_sync/widgets/common/modal_progress_indicator.dart';
+import 'package:provider/provider.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AuthScreen(
+      title: I18n.singupScreenTitle,
+      mainButtonText: I18n.singupScreenMainButtonText,
+      onMainButtonPressed: (email, password) async {
+        print('signup, $email : $password');
+        ModalProgressIndicator.show(context);
+        final success = await context.read<IAuthService>().createUserAccount(email: email, password: password);
+        ModalProgressIndicator.dismiss();
+        if (success) {
+          await UserPreferences.setUsername(email);
+          await Navigator.of(context).pushReplacementNamed(RouteNames.homeScreen);
+        }
+      },
+      secondaryButtonText: I18n.singupScreenSecondaryButtonText,
+      onSecondaryButtonPressed: () => Navigator.of(context).pushReplacementNamed(RouteNames.loginScreen),
+    );
+  }
+}
