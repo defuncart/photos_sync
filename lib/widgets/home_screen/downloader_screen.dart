dde092122a0bd55c33b5e39650f44215a739f81a
James Leahy <defuncart@gmail.com>
2020-08-18 17:25:28 +0200

    Add DownloaderScreen.

---
+++ lib/widgets/home_screen/downloader_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DownloaderScreen extends StatelessWidget {
+  const DownloaderScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('DownloaderScreen'),
+    );
+  }
+}
