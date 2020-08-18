48f5dd9099fb0cfbae53ec1731ebbb58052b2942
James Leahy <defuncart@gmail.com>
2020-08-18 17:26:12 +0200

    Update HomeScreen.
    Desktop is considered downloader, mobile uploader.

--- lib/widgets/home_screen/home_screen.dart
+++ lib/widgets/home_screen/home_screen.dart
@@ -1,14 +1,12 @@
-import 'dart:io';
-
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
 import 'package:photos_sync/configs/route_names.dart';
+import 'package:photos_sync/enums/client_type.dart';
 import 'package:photos_sync/i18n.dart';
 import 'package:photos_sync/modules/backend/backend.dart';
 import 'package:photos_sync/modules/user_preferences/user_preferences.dart';
-import 'package:photos_sync/widgets/common/custom_button.dart';
-import 'package:photos_sync/widgets/common/error_dialog.dart';
+import 'package:photos_sync/utils/device_utils.dart';
+import 'package:photos_sync/widgets/home_screen/downloader_screen.dart';
+import 'package:photos_sync/widgets/home_screen/uploader_screen.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -56,92 +54,31 @@ class HomeScreen extends StatelessWidget {
 }
 
 class _HomePageContent extends StatefulWidget {
-  const _HomePageContent({
-    Key key,
-  }) : super(key: key);
+  const _HomePageContent({Key key}) : super(key: key);
 
   @override
   __HomePageContentState createState() => __HomePageContentState();
 }
 
 class __HomePageContentState extends State<_HomePageContent> {
-  bool _isUploading = false;
-  int _photosUploaded;
-  int _photosToUpload;
-
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: _isUploading
-            ? Text(I18n.homeScreenUploadingText(completed: _photosUploaded, total: _photosToUpload))
-            : CustomButton(
-                buttonText: I18n.homeScreenChoosePhotosButtonText,
-                onPressed: () async {
-                  final files = await FilePicker.getMultiFile(
-                    type: FileType.image,
-                  );
-
-                  if (files != null && files.isNotEmpty) {
-                    final photosOnServer =
-                        await context.read<IDatabaseService>().getPhotos(user: UserPreferences.getUsername());
-                    final photosToUpload = <_PhotoToUpload>[];
-                    for (final file in files) {
-                      final photo = SyncedPhoto(
-                        user: UserPreferences.getUsername(),
-                        folder: dirname(file.path).split('/').last,
-                        filename: basename(file.path),
-                        absoluteFilepath: file.path,
-                      );
-                      if (!photosOnServer.contains(photo)) {
-                        photosToUpload.add(_PhotoToUpload(file: file, photo: photo));
-                      } else {
-                        // TODO notify user already on server
-                      }
-                    }
-
-                    if (photosToUpload.isNotEmpty) {
-                      await _uploadPhotos(photosToUpload, context);
-                    }
-                  }
-                },
-              ),
-      ),
-    );
-  }
-
-  Future<void> _uploadPhotos(List<_PhotoToUpload> photosToUpload, BuildContext context) async {
-    setState(() {
-      _isUploading = true;
-      _photosUploaded = 0;
-      _photosToUpload = photosToUpload.length;
-    });
-
-    for (final photoToUpload in photosToUpload) {
-      final success = await context.read<ISyncService>().uploadFile(photoToUpload.file, photo: photoToUpload.photo);
-      if (!success) {
-        showErrorDialog(context);
-        break;
-      }
-
-      setState(() {
-        _photosUploaded++;
-      });
+    final clientType = UserPreferences.getClientType();
+    if (clientType == null) {
+      _initializeClientType();
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      return clientType == ClientType.uploader ? UploaderScreen() : DownloaderScreen();
     }
+  }
 
-    setState(() {
-      _isUploading = false;
+  void _initializeClientType() {
+    Future.microtask(() async {
+      final clientType = DeviceUtils.isMobile ? ClientType.uploader : ClientType.downloader;
+      await UserPreferences.setClientType(clientType);
+      setState(() {});
     });
   }
-}
-
-class _PhotoToUpload {
-  final File file;
-  final SyncedPhoto photo;
-
-  const _PhotoToUpload({
-    @required this.file,
-    @required this.photo,
-  })  : assert(file != null),
-        assert(photo != null);
 }
